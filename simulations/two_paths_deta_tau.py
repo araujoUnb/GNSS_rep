cd4f36d9a0ea733e85b6d1979e7e77620ab65689
Daniel Araújo <daniel.araujo@unb.br>
2020-06-25 13:59:44 -0300

    results with angle estimation

--- simulations/two_paths_deta_tau.py
+++ simulations/two_paths_deta_tau.py
@@ -5,7 +5,7 @@ from gnss_func.estimators import singlePolModel_estimator
 
 from os import path
 
-MC = 20
+MC = 60
 #%%
 
 nAntennas = 8
@@ -21,7 +21,7 @@ delayGranularity = 11
 nColumns = 11
 factor = np.linspace(0,1,nColumns)
 
-Folder = 'deltaTau/'
+Folder = 'deltaTau_angle/'
 CN = 48
 
 
@@ -53,7 +53,7 @@ for ii in range(nColumns):
             gnss_model.rx_signal(CN)
 
             estimator_model = singlePolModel_estimator(gnss_model, delayGranularity, theta_deg_space)
-            rmse = estimator_model.rmse(tau_los)
+            rmse = estimator_model.rmse_angle(tau_los)
 
             results_dic = {'rmse':[rmse],
                            'tau_los':[tau_los],
